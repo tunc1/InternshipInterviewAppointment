b76f2f87bd3343faf8f2ca03de18404710046169
user <user@a.com>
2020-05-31 18:14:35 +0200

    Changed JSON Output of InternshipType.java

--- src/main/java/app/entity/InternshipType.java
+++ src/main/java/app/entity/InternshipType.java
@@ -1,8 +1,12 @@
 package app.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@JsonFormat(shape=JsonFormat.Shape.OBJECT)
+@Getter
 public enum InternshipType
 {
     HARDWARE("Donanım"),SOFTWARE("Yazılım");
