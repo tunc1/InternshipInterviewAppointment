a1cadccc874a277e47e08a48969cf69ee9b90ec1
user <user@a.com>
2020-06-14 01:36:53 +0200

    Changed InternshipLocation

--- src/main/java/app/entity/InternshipLocation.java
+++ src/main/java/app/entity/InternshipLocation.java
@@ -1,6 +1,8 @@
 package app.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +11,21 @@ import lombok.Getter;
 @Getter
 public enum InternshipLocation
 {
-    ERASMUS("Erasmus"),ABROAD("Yurtdışı"),DOMESTIC("Yurtiçi");
+    ERASMUS("Erasmus"),ABROAD("Abroad"),DOMESTIC("Domestic");
     private String name;
+    @JsonCreator
+    public static InternshipLocation creator(@JsonProperty String name)
+    {
+        switch(name.toLowerCase())
+        {
+            case "erasmus":
+                return ERASMUS;
+            case "abroad":
+                return ABROAD;
+            case "domestic":
+                return DOMESTIC;
+            default:
+                return null;
+        }
+    }
 }
