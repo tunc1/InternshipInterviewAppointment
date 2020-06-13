4accd700e84abaaac588911731faf260bee36045
user <user@a.com>
2020-06-14 01:39:24 +0200

    Changed InternshipType

--- src/main/java/app/entity/InternshipType.java
+++ src/main/java/app/entity/InternshipType.java
@@ -1,6 +1,8 @@
 package app.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,4 +13,17 @@ public enum InternshipType
 {
     HARDWARE("Hardware"),SOFTWARE("Software");
     private String name;
+    @JsonCreator
+    public static InternshipType creator(@JsonProperty String name)
+    {
+        switch(name.toLowerCase())
+        {
+            case "hardware":
+                return HARDWARE;
+            case "software":
+                return SOFTWARE;
+            default:
+                return null;
+        }
+    }
 }
