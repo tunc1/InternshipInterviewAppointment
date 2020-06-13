3191fac272bdc30b1c87c29bad0afb7ceebf9504
user <user@a.com>
2020-06-14 01:38:40 +0200

    Changed InternshipNote

--- src/main/java/app/entity/InternshipNote.java
+++ src/main/java/app/entity/InternshipNote.java
@@ -1,6 +1,29 @@
 package app.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@JsonFormat(shape=JsonFormat.Shape.OBJECT)
+@Getter
 public enum InternshipNote
 {
-    YT,YZ
+    YT("YT"),YZ("YZ");
+    private String note;
+    @JsonCreator
+    public static InternshipNote creator(@JsonProperty String name)
+    {
+        switch(name.toLowerCase())
+        {
+            case "yt":
+                return YT;
+            case "yz":
+                return YZ;
+            default:
+                return null;
+        }
+    }
 }
