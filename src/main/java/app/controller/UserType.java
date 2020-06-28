7f56317baf7206b263a22b7c70614c4efac76bf7
user <user@a.com>
2020-06-28 18:02:33 +0200

    Added UserType.java

---
+++ src/main/java/app/controller/UserType.java
@@ -0,0 +1,29 @@
+package app.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userType")
+@CrossOrigin("*")
+public class UserType
+{
+    @PostMapping
+    public Response userType(Authentication authentication)
+    {
+        return new Response(authentication.getPrincipal().getClass().getSimpleName());
+    }
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    private static class Response
+    {
+        private String userType;
+    }
+}
