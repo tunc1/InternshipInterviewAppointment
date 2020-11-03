5b3c42f6ffd3b96a778c3b8f170130ffc569348e
User <user@a.com>
2020-11-04 00:02:46 +0200

    Added existsByUsername to TeacherRepository

--- src/main/java/app/repository/TeacherRepository.java
+++ src/main/java/app/repository/TeacherRepository.java
@@ -1,9 +1,11 @@
 package app.repository;
 
 import app.entity.Teacher;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeacherRepository extends JpaRepository<Teacher,Integer>
 {
     Teacher findByUsername(String username);
+    boolean existsByUsername(String username);
 }
