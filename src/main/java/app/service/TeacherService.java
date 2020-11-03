2d6aa7d661497995bc734f19220942a4db5664f0
User <user@a.com>
2020-11-04 00:05:57 +0200

    Added addNewTeacher to TeacherService

--- src/main/java/app/service/TeacherService.java
+++ src/main/java/app/service/TeacherService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service
@@ -39,5 +40,16 @@ public class TeacherService
     public Teacher findByUsername(String username)
     {
         return teacherRepository.findByUsername(username);
+    }
+    @PostConstruct
+    public void addNewTeacher()
+    {
+        if(!teacherRepository.existsByUsername("admin"))
+        {
+            Teacher teacher=new Teacher();
+            teacher.setUsername("admin");
+            teacher.setPassword("password");
+            save(teacher);
+        }
     }
 }
