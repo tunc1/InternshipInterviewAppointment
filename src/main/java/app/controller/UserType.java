e4690276532a43bed29dc1d2ee5f31437452f57d
user <email>
2021-10-28 21:00:52 +0300

    Update UserType.java Switch to Get from Post

--- src/main/java/app/controller/UserType.java
+++ src/main/java/app/controller/UserType.java
@@ -2,6 +2,7 @@ package app.controller;
 
 import app.response.UserTypeResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/userType")
 public class UserType
 {
-    @PostMapping
+    @GetMapping
     public UserTypeResponse userType(Authentication authentication)
     {
         return new UserTypeResponse(authentication.getPrincipal().getClass().getSimpleName());
