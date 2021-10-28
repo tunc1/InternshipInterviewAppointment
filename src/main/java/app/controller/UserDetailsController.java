e55ae40147ea961d4559f517e13fce9f9eceedde
user <email>
2021-10-28 20:59:47 +0300

    Update UserDetailsController Switch to Get from Post

--- src/main/java/app/controller/UserDetailsController.java
+++ src/main/java/app/controller/UserDetailsController.java
@@ -2,7 +2,7 @@ package app.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/userDetails")
 public class UserDetailsController
 {
-    @PostMapping
+    @GetMapping
     public UserDetails get(Authentication authentication)
     {
         return (UserDetails)authentication.getPrincipal();
