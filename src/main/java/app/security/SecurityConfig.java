04833ff9e81ccf9470598ac94dbf101afbf2ac12
user <email>
2022-06-05 19:30:43 +0300

    Version Update

---
+++ src/main/java/app/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package app.security;
+
+import app.consts.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig
+{
+    @Autowired
+    private TokenFilter tokenFilter;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth ->
+                        auth.antMatchers("/login/**").permitAll()
+                        .antMatchers(HttpMethod.GET,"/internshipSubject/**","/appointment/**","/announcement/**").authenticated()
+                        .antMatchers(HttpMethod.PUT,"/appointment/**").authenticated()
+                        .antMatchers(HttpMethod.POST,"/internship/**").authenticated()
+                        .antMatchers(HttpMethod.GET,"/internship/student/**").authenticated()
+                        .antMatchers("/teacher/**","/student/**","/appointment/**","/internship/**","/internshipSubject/**","/announcement/**").hasRole(Role.TEACHER)
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(tokenFilter,UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception
+    {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
